167afc1d7795978a24693533a3deb5473903591a
dnn004 <dnn004@ucsd.edu>
2020-08-08 02:55:10 -0700

    Assert Fail

--- test_main.py
+++ test_main.py
@@ -19,6 +19,7 @@ def client():
     client = main.APP.test_client()
 
     yield client
+    assert False
 
 
 
@@ -26,7 +27,7 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
-
+    assert False
 
 def test_auth(client):
     body = {'email': EMAIL,
@@ -38,3 +39,4 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+    assert False
